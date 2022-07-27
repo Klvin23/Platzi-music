522bb2c74d4da3dbbc2f9fdbbac0239071034a30
klvin23 <klvin.g23@gmail.com>
2022-07-27 18:23:18 -0500

    creacion de codigo funcional para app platzi-music

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Inicios en Vue.js"/>
+<template lang="pug">
+#app
+    section.section
+      nav.navbar
+        .field.has-addons
+          .control.is-expanded
+            input.input(
+              type="text"
+              placeholder="Buscar canciones"
+              v-model="nombre")
+          .control
+            button.button.is-info(@click="buscar") Buscar
+          .control
+            button.button.is-danger &times;
+          .control
+            button.button
+              span.is-size-7 Encontrado: {{ cantidad }}
+
+      .container.custom
+        .columns
+          .colum(v-for="c in canciones") {{ c.nombre }},{{ c.artista }}
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const canciones = [
+  { 'nombre': 'De ves en mes', 'artista': 'Ricardo Arjona' },
+  { 'nombre': 'Pinguinos en la cama', 'artista': 'Ricardo Arjona' },
+  { 'nombre': 'Atrevete', 'artista': 'Calle 13' },
+  { 'nombre': 'Nadie como tu', 'artista': 'Calle 13' }
+]
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      canciones: [],
+      nombre: ''
+    }
+  },
+  methods: {
+    buscar () {
+      this.canciones = canciones
+    }
+  },
+  computed: {
+    cantidad () {
+      return this.canciones.length
+    }
   }
 }
 </script>
 
-
-<style>
-
+<style lang="scss">
+@import './components/scss/main.scss';
 
 #app {
 
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .custom {
+    margin-top: 30px;
+  }
 
-  
 }
 </style>
