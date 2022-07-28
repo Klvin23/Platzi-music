f0029121fc3160dfa40d4628d78b7431fc5a0d55
klvin23 <klvin.g23@gmail.com>
2022-07-28 18:00:46 -0500

    cambios en archivo src/components/HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,64 +1,35 @@
-<template lang="pug">
-#app
-    section.section
-      nav.navbar
-        .field.has-addons
-          .control.is-expanded
-            input.input(
-              type="text"
-              placeholder="Buscar canciones"
-              v-model="nombre")
-          .control
-            button.button.is-info(@click="buscar") Buscar
-          .control
-            button.button.is-danger &times;
-          .control
-            button.button
-              span.is-size-7 Encontrado: {{ cantidad }}
-
-      .container.custom
-        .columns
-          .colum(v-for="c in canciones") {{ c.nombre }},{{ c.artista }}
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
 </template>
 
 <script>
-
-const canciones = [
-  { 'nombre': 'De ves en mes', 'artista': 'Ricardo Arjona' },
-  { 'nombre': 'Pinguinos en la cama', 'artista': 'Ricardo Arjona' },
-  { 'nombre': 'Atrevete', 'artista': 'Calle 13' },
-  { 'nombre': 'Nadie como tu', 'artista': 'Calle 13' }
-]
-
 export default {
-  name: 'app',
-  data () {
-    return {
-      canciones: [],
-      nombre: ''
-    }
-  },
-  methods: {
-    buscar () {
-      this.canciones = canciones
-    }
-  },
-  computed: {
-    cantidad () {
-      return this.canciones.length
-    }
+  name: 'HelloWorld',
+  props: {
+    msg: String
   }
 }
 </script>
 
-<style lang="scss">
-@import './components/scss/main.scss';
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 
-#app {
 
-  .custom {
-    margin-top: 30px;
-  }
 
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
