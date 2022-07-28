581624a45500399037044c3ad52a1d9c8373acc1
klvin23 <klvin.g23@gmail.com>
2022-07-28 18:58:53 -0500

    se crea el archivo platzi-music.js

---
+++ src/servicios/platzi-music.js
@@ -0,0 +1,8 @@
+import axios, { axios } from "axios";
+import configService from './config'
+
+const platziMusicService = axios.create ({
+    baseUrl: configService.apiUrl
+})
+
+export default platziMusicService
